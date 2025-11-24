08536ca2e7ba049327100f1febadeafc30abc796
Bruno Galdino <brunogaldinoc@gmail.com>
2025-11-24 17:17:51 -0300

    feat: added custom catppuccin-gruvbox theme and tiny-inline-diagnostic

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,31 +1,32 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        vtsls = {
-          settings = {
-            typescript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              preferences = {
-                importModuleSpecifier = "project-relative",
-                importModuleSpecifierEnding = "minimal",
-              },
-            },
-          },
-        },
-      },
-    },
-    setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
-    },
-  },
-}
+return {}
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         vtsls = {
+--           settings = {
+--             typescript = {
+--               updateImportsOnFileMove = { enabled = "always" },
+--               preferences = {
+--                 importModuleSpecifier = "project-relative",
+--                 importModuleSpecifierEnding = "minimal",
+--               },
+--             },
+--           },
+--         },
+--       },
+--     },
+--     setup = {
+--       eslint = function()
+--         require("lazyvim.util").lsp.on_attach(function(client)
+--           if client.name == "eslint" then
+--             client.server_capabilities.documentFormattingProvider = true
+--           elseif client.name == "tsserver" then
+--             client.server_capabilities.documentFormattingProvider = false
+--           end
+--         end)
+--       end,
+--     },
+--   },
+-- }
